980272f15ec19dbdb1cb09f87adf91cf0505034f
J3F31 <72414263+J3F31@users.noreply.github.com>
2021-11-23 18:37:00 +0100

    Expected vessel list (1 row)

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,8 @@
 <template lang="html">
   <div class="px-4 py-6">
     <filter-header />
+    <expected />
+    
 
     <nuxt />
   </div>
@@ -8,8 +10,9 @@
 
 <script lang="js">
 import FilterHeader from '~/components/FilterHeader'
+import Expected from '~/components/Expected'
 export default {
-  components: { FilterHeader },
+  components: { FilterHeader, Expected},
   head: {
     bodyAttrs: {
       class: 'bg-gray-100 dark:bg-gray-800 dark:text-white'
