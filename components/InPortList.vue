c67d03812b4089c75c3ac6d01c3aaaa0683bfdc3
Laurens Dijkstra <laurensdijkstra1@gmail.com>
2021-12-01 14:19:26 +0100

    Fixed that you can have icons inside the header table

--- components/InPortList.vue
+++ components/InPortList.vue
@@ -4,6 +4,22 @@
       :headers="headers"
       :data="data"
     >
+      <template v-slot:thead="props">
+        <thead :class="props.theadClass">
+          <tr :class="props.trClass">
+            <th
+              v-for="(item, index) in props.data"
+              :key="index"
+              :class="[props.thClass, item.icon ? 'px-1' : 'px-2']"
+            >
+              <template v-if="item.text">
+                {{ item.text }}
+              </template>
+              <font-awesome-icon v-else-if="item.icon" :icon="item.icon" :class="`text-${item.color}-400`" class="ml-4 w-4" />
+            </th>
+          </tr>
+        </thead>
+      </template>
 
       <template slot="row" slot-scope="props">
         <tr :class="props.trClass">
@@ -54,7 +70,7 @@
           <td :class="props.tdClass">
             {{ props.row[14] }}
           </td>
-<!--          -->
+          <!--          -->
           <td class="px-1" :class="props.tdClass">
             <!-- <icon color="green" :name="props.row[15]" /> -->
             <font-awesome-icon :icon="props.row[15]" class="ml-4 w-4 text-green-400" />
@@ -78,158 +94,105 @@
 
 // import Icon from '~/components/Icon'
 
-  const headers = [
-    {
-      id: 'type',
-      value: 'type',
-      text: 'Type',
-      className: 'bg-gray-100',
-    },
-    {
-      id: 'etd',
-      value: 'etd',
-      text: 'ETD',
-      className: 'bg-gray-100',
-    },
-    {
-      id: 'Vessel',
-      value: 'Vessel',
-      text: 'Vessel',
-      className: 'bg-gray-100',
-    },
-    {
-      id: 'Berth',
-      value: 'Berth',
-      text: 'Berth',
-      className: 'bg-gray-100',
-    },
-    {
-      id: 'Mooring',
-      value: 'Mooring',
-      text: 'Mooring',
-      className: 'bg-gray-100',
-    },
-    {
-      id: 'B',
-      value: 'B',
-      text: 'B',
-      className: 'bg-gray-100 px-1',
-    },
-    {
-      id: 'T',
-      value: 't',
-      text: 'T',
-      className: 'bg-gray-100 px-1',
-    },
-    {
-      id: 'P',
-      value: 'p',
-      text: 'P',
-      className: 'bg-gray-100 px-1',
-    },
-    {
-      id: 'LOA',
-      value: 'LOA',
-      text: 'LOA',
-      className: 'bg-gray-100',
-    },
-    {
-      id: 'BOA',
-      value: 'BOA',
-      text: 'BOA',
-      className: 'bg-gray-100',
-    },
-    {
-      id: 'Draft',
-      value: 'Draft',
-      text: 'Draft',
-      className: 'bg-gray-100',
-    },
-    {
-      id: 'H',
-      value: 'H',
-      text: 'H',
-      className: 'bg-gray-100',
-    },
-    {
-      id: 'ETA',
-      value: 'ETA',
-      text: 'ETA',
-      className: 'bg-gray-100',
-    },
-    {
-      id: 'BERTH',
-      value: 'BERTH',
-      text: 'BERTH',
-      className: 'bg-gray-100',
-    },
-    {
-      id: 'MOORING',
-      value: 'MOORING',
-      text: 'MOORING',
-      className: 'bg-gray-100',
-    },
-    {
-      id: 'ICON',
-      value: 'ICON',
-      text: 'ICON',
-      className: 'bg-gray-100',
-    },
-    {
-      id: 'ICON',
-      value: 'ICON',
-      text: 'ICON',
-      className: 'bg-gray-100',
-    },
-    {
-      id: 'ICON',
-      value: 'ICON',
-      text: 'ICON',
-      className: 'bg-gray-100',
-    },
-    {
-      id: 'ICON',
-      value: 'ICON',
-      text: 'ICON',
-      className: 'bg-gray-100',
-    },
-  ]
-  const data = [
-    [
-      'Type',
-      'ETD',
-      'Vessel',
-      'Berth',
-      'Mooring',
-      'B',
-      'T',
-      'P',
-      'LOA',
-      'BOA',
-      'Draft',
-      'H',
-      'ETA',
-      'BERTH',
-      'MOORING',
-      'award',
-      'recycle',
-      'paperclip',
-      'pen',
-    ],
-    [
-      'A', 
-      '15:00', 
-      'EST', 
-      '0.0?'
-    ],
-  ]
+const headers = [
+  {
+    text: 'Type',
+  },
+  {
+    text: 'ETD',
+  },
+  {
+    text: 'Vessel',
+  },
+  {
+    text: 'Berth',
+  },
+  {
+    text: 'Mooring',
+  },
+  {
+    text: 'B',
+  },
+  {
+    text: 'T',
+  },
+  {
+    text: 'P',
+  },
+  {
+    text: 'LOA',
+  },
+  {
+    text: 'BOA',
+  },
+  {
+    text: 'Draft',
+  },
+  {
+    text: 'H',
+  },
+  {
+    text: 'ETA',
+  },
+  {
+    text: 'BERTH',
+  },
+  {
+    text: 'MOORING',
+  },
+  {
+    icon: 'award',
+    color: 'green'
+  },
+  {
+    icon: 'recycle',
+    color: 'red'
+  },
+  {
+    icon: 'paperclip',
+    color: 'gray'
+  },
+  {
+    icon: 'pen',
+    color: 'yellow'
+  },
+]
+const data = [
+  [
+    'Type',
+    'ETD',
+    'Vessel',
+    'Berth',
+    'Mooring',
+    'B',
+    'T',
+    'P',
+    'LOA',
+    'BOA',
+    'Draft',
+    'H',
+    'ETA',
+    'BERTH',
+    'MOORING',
+    'award',
+    'recycle',
+    'paperclip',
+    'pen',
+  ],
+  [
+    'A',
+    '15:00',
+    'EST',
+    '0.0?'
+  ],
+]
 
-  export default {
-    // components: { Icon },
+export default {
+  // components: { Icon },
 
-    data: () => ({
-      headers,
-      data
-    }),
-  }
+  data: () => ({
+    headers,
+    data
+  }),
+}
 </script>
